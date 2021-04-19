d393b345374ae37fb143fa6c804662ec8b14c1c1
Exr0n <mail@exr0n.com>
2021-04-18 20:19:05 -0700

    init grid renderer, should hopefully be reasonably sped

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,30 @@
 #include <cstdio>
 #include <cstdint>
+#include <cstdlib>
 
 // TODO: naive impl
 // TODO: count on update
 // TODO: spfa
 // TODO: use 4bit representation
 
-const size_t MX = 128;
+using ut = std::uint_fast32_t;
+const size_t MX = 64-2;
 
-uint8_t grid[MX][MX];
+std::uint_fast8_t grid[MX+2][MX+2];
+
+const char output_lookup[] = { ' ', '#' };
+void render_grid()
+{
+    for (ut i=1; i<MX; ++i) {
+        for (ut j=1; j<MX; ++j)
+            putchar_unlocked(output_lookup[grid[i][j]&1]);
+        putchar_unlocked('\n');
+    }
+}
 
 int main()
 {
     puts("hello world");
+    render_grid();
 }
 
