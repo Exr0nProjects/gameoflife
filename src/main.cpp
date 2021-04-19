6ba54f0ac5b762ef855cbc7707afe56a2cd23d2c
Exr0n <mail@exr0n.com>
2021-04-18 20:50:12 -0700

    refactor out to a set_alive and set_dead fxns

--- src/main.cpp
+++ src/main.cpp
@@ -24,19 +24,33 @@ void render_grid()
     }
 }
 
+void set_alive(nt y, nt x)
+{
+    // OPTM: this code is probably quite hot
+    grid[y][x] |= 1;                    // set cell as alive
+    for (nt dx=-1; dx<2; ++dx)
+        for (nt dy=-1; dy<2; ++dy)
+            grid[y+dy][x+dx] += 2;      // add one to each surrounding cell
+    grid[y][x] -= 2;                    // don't add one to self
+}
+
+void set_dead(nt y, nt x)
+{
+    // OPTM: this code is probably quite hot
+    grid[y][x] &= 0;                    // set cell as dead
+    for (nt dx=-1; dx<2; ++dx)
+        for (nt dy=-1; dy<2; ++dy)
+            grid[y+dy][x+dx] -= 2;      // subtract one from each surrounding cell
+    grid[y][x] += 2;                    // don't subtract one from self
+}
+
 void init_grid()
 {
     std::srand(0x326ccc); // :sunglasses:
     for (nt i=1; i<=MX; ++i) {
         for (nt j=1; j<=MX; ++j) {
             nt g = rand();
-            if (g&1) {
-                grid[i][j] |= 1;                    // set cell as alive
-                for (nt dx=-1; dx<2; ++dx)
-                    for (nt dy=-1; dy<2; ++dy)
-                        grid[i+dx][j+dy] += 2;      // add one to each surrounding cell
-                grid[i][j] -= 2;                    // don't add one to self
-            }
+            if (g&1) set_alive(i, j);
         }
     }
 }
